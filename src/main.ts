1ecb4994ed2f536c235394213fc692713efaee5a
wofty <sorin.mihalachee@gmail.com>
2023-12-31 14:58:36 +0000

    Added new pages and updated Tailwind CSS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 import './style.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleButton = document.getElementById('themeToggleButton');
+    const htmlElement = document.documentElement;
+
+    // Initialize theme based on local storage or system preference
+    initializeTheme();
+
+    themeToggleButton?.addEventListener('click', function () {
+        const isDarkMode = htmlElement.classList.toggle('dark');
+        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    });
+
+    function initializeTheme() {
+        const storedTheme = localStorage.getItem('theme');
+        const prefersDark = window.matchMedia(
+            '(prefers-color-scheme: dark)'
+        ).matches;
+
+        if (storedTheme) {
+            htmlElement.classList.toggle('dark', storedTheme === 'dark');
+        } else {
+            htmlElement.classList.toggle('dark', prefersDark);
+            localStorage.setItem('theme', prefersDark ? 'dark' : 'light');
+        }
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,37 @@
 export default {
   darkMode: 'class',
   content: [
-    "./index.html",
+    "./**.{html,js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#102756',
+        'secondary': '#dc6434',
+        'ecru-white': '#fbfbf4',
+        'gray-chateau': '#9da5b2',
+        'apricot': '#e79979',
+        'cashmere': '#e4b6a4',
+        'regent-gray': '#7c8c9c',
+        'ghost': '#cbccd1',
+        'mandys-pink': '#f3c7b4'
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+        'serif': ['Merriweather', 'serif'],
+        'cursive': ['Playball', 'cursive'],
+
+      },
+      backgroundImage: {
+        'hero-about': "url('images/about/hero-about-test.jpg')",
+        'menu-bg-hover': "url('shapes/menu-stroke.png')"
+      },
+
+
+    }
   },
+
   plugins: [],
 }
 
