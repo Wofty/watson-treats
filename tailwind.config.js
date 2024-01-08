f918ef59eeaa70144885e24dc2199745fbb80046
wofty <sorin.mihalachee@gmail.com>
2024-01-08 08:27:11 +0000

    Delete about-bg-s3.png and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,13 +36,6 @@ export default {
         'home-hero-bg': "url('/images/home/hero-bg.webp')",
         //section bg
         'section-bg-1': "url('/images/bg/section_bg-1.png')",
-
-
-        // home bg
-        'home-sec1-bg': "url('/images/home/home-bg-s1-light.png')",
-        'home-sec3-bg': "url('/images/home/bg-home.png')",
-
-
         //separators
         'separator-1': "url('/shapes/separator_02.png')",
         'separator-2': "url('/shapes/separator_02.png')",
